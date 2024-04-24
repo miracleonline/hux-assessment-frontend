6f96feb1c0ae992f828bf3fea68071c9d06e84cd
Miracle Nwabuisi <110323526+miracleonline@users.noreply.github.com>
2024-04-24 01:23:53 -1200

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Contacts App</h1>
+      <p>
+        Contacts App is a simple web application that allows you to manage your contact information conveniently.
+      </p>
+      <p>
+        With Contacts App, you can easily add, edit, and delete contacts, as well as view detailed information about each contact.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
