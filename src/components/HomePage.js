782aa3c46d5738aa2f48049bd2553107e5463ff0
Miracle Nwabuisi <110323526+miracleonline@users.noreply.github.com>
2024-04-24 01:29:04 -1200

    Create HomePage.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Contacts App</h1>
+      <p>This is a simple web application for managing your contacts.</p>
+      <p>Please sign up or log in to get started.</p>
+    </div>
+  );
+};
+
+export default HomePage;
